c7f0722ee583abde0f19cc35d255b58045a7bc58
BRaines91 <70935080+BRaines91@users.noreply.github.com>
2020-10-27 02:19:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 
+let city=""; 
+let url="";
+let APIkey="";
+let queryurl ="";
+let currenturl = "";
+let citiesDiv = document.getElementById("searched_cities_container");
+let cities = []; 
+init(); 
+listClicker(); 
+searchClicker(); 
+
+function init(){
+    let saved_cities = JSON.parse(localStorage.getItem("cities"));
+
+    if (saved_cities !== null){
+        cities = saved_cities
+    }   
+    
+    renderButtons(); 
+}
+
+function storeCities(){
+    localStorage.setItem("cities", JSON.stringify(cities)); 
+}
+
+
+function renderButtons(){
+    citiesDiv.innerHTML = ""; 
+    if(cities == null){
+        return;
+    }
+    let unique_cities = [...new Set(cities)];
+    for(let i=0; i < unique_cities.length; i++){
+        let cityName = unique_cities[i]; 
+
+        let buttonEl = document.createElement("button");
+        buttonEl.textContent = cityName; 
+        buttonEl.setAttribute("class", "listbtn"); 
+
+        citiesDiv.appendChild(buttonEl);
+        listClicker();
